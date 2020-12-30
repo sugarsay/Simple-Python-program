2a9f934380764e7903c1cbb8b1e1062f5576e323
Sugarsay Tutorials <76730729+sugarsay@users.noreply.github.com>
2020-12-30 11:43:23 +0100

    Add files via upload

---
+++ simple_program.py
@@ -0,0 +1,7 @@
+print ("Welcome to this simple program")
+name = input("What is your name? ")
+print ("Hello " + name)
+age = input("How old are you? ")
+home = input("Where do you come from? ")
+animal = input("What is your favourite animal? ")
+print (name + " so you are " + age + " years old, you come from " + home + " and your favorite animal is " + animal)
